e7a54909d4647a5c4e6890763c373329983d5cf1
karthikeya <karthikeya777666@gmail.com>
2025-12-30 15:33:50 -0500

    Add convert button callback and improve UI layout
    Introduced a convert() function as the button's command callback and adjusted the window size. Improved input and button layout, added padding, and included an output label for displaying results.

--- Converter.py
+++ Converter.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
+def convert():
+    print("convert")
+
 # window
 window = tk.Tk()
 window.title("Converter")
-window.geometry('500x200')
+window.geometry('400x200')
 
 #title
 title_label = ttk.Label(master=window, text="Pounds to Kilograms", font="Minion 25 bold")
@@ -13,10 +16,14 @@ title_label.pack()
 #input frame
 input_frame = ttk.Frame(master=window)
 input = ttk.Entry(master=input_frame)
-button = ttk.Button(master=input_frame, text='convert')
-input.pack()
-button.pack()
-input_frame.pack()
+button = ttk.Button(master=input_frame, text='convert', command=convert) 
+input.pack(side='left', padx=5)
+button.pack(side='left')
+input_frame.pack(pady=20)
+
+#output
+output_label = ttk.Label(master=window, text = 'Output', font="Minion 25")
+output_label.pack()
 
 #call/run
 window.mainloop()
