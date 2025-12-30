e6b8951140f5591e9fcecb696f79c358e8a24a35
karthikeya <karthikeya777666@gmail.com>
2025-12-30 15:13:05 -0500

    Add input frame with entry and button to UI
    Introduced an input frame containing an entry widget and a convert button to the main window, preparing the interface for user input and conversion functionality.

--- Converter.py
+++ Converter.py
@@ -10,5 +10,13 @@ window.geometry('500x200')
 title_label = ttk.Label(master=window, text="Pounds to Kilograms", font="Minion 25 bold")
 title_label.pack()
 
+#input frame
+input_frame = ttk.Frame(master=window)
+input = ttk.Entry(master=input_frame)
+button = ttk.Button(master=input_frame, text='convert')
+input.pack()
+button.pack()
+input_frame.pack()
+
 #call/run
 window.mainloop()
